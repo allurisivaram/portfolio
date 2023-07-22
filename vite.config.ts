25f75213e8e75c32e5a6a12bddd7d88544e96227
allurisivaram <allurisivaram@outlook.com>
2023-07-22 11:52:14 -0400

    Update vite.config.ts
    Icons path changed

--- vite.config.ts
+++ vite.config.ts
@@ -9,28 +9,46 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     name:'niva-app',
     short_name:'niva-app',
     description: 'An app that helps you to find shared home in canada',
-    icons:[
+    "icons": [
       {
-        src:'/android-chrome-192-192.png',
-        sizes:'192x192',
-        type:'image/png'
+        "src": "assets/icons/icon-72x72.png",
+        "sizes": "72x72",
+        "type": "image/png"
       },
       {
-        src:'/android-chrome-512-512.png',
-        sizes:'512x512',
-        type:'image/png'
+        "src": "assets/icons/icon-96x96.png",
+        "sizes": "96x96",
+        "type": "image/png"
       },
       {
-        src:'/apple-touch-icon.png',
-        sizes:'180x180',
-        type:'image/png',
-        purpose:'apple-touch-icon'
+        "src": "assets/icons/icon-128x128.png",
+        "sizes": "128x128",
+        "type": "image/png"
       },
       {
-        src:'/maskable_icon.png',
-        sizes:'225x225',
-        type:'image/png',
-        purpose:'any maskable'
+        "src": "assets/icons/icon-144x144.png",
+        "sizes": "144x144",
+        "type": "image/png"
+      },
+      {
+        "src": "assets/icons/icon-152x152.png",
+        "sizes": "152x152",
+        "type": "image/png"
+      },
+      {
+        "src": "assets/icons/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "assets/icons/icon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png"
+      },
+      {
+        "src": "assets/icons/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
       }
     ],
     theme_color: '#171717',
@@ -43,7 +61,7 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
   workbox: {
     runtimeCaching: [{
       urlPattern:({url}) => {
-        return url.pathname.startsWith('/properties');
+        return url.pathname.startsWith('https://realty-in-ca1.p.rapidapi.com')
       },
       handler:'CacheFirst' as const,
       options: {
